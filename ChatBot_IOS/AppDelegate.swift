c1b51bcc96810dd4106dc2aeb46d23afb099347d
Darya95 <darya.isakova.95@gmail.com>
2019-04-02 01:25:00 +0300

    Work process

--- ChatBot_IOS/AppDelegate.swift
+++ ChatBot_IOS/AppDelegate.swift
@@ -1,46 +1,136 @@
 //
 //  AppDelegate.swift
-//  ChatBot_IOS
+//  Kommunicate
 //
-//  Created by Darya Isakova on 01/04/2019.
-//  Copyright © 2019 Дарья Исакова. All rights reserved.
+//  Created by mukeshthawani on 02/19/2018.
+//  Copyright (c) 2018 mukeshthawani. All rights reserved.
 //
 
 import UIKit
+import UserNotifications
+import Kommunicate
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    // Pass your App Id here. You can get the App Id from install section in the dashboard.
+    static let appId = "5127d7fface892059ea4ea4c3a86f056"
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        registerForNotification()
+        
+        KMPushNotificationHandler.shared.dataConnectionNotificationHandlerWith(Kommunicate.defaultConfiguration)
+        let kmApplocalNotificationHandler : KMAppLocalNotification =  KMAppLocalNotification.appLocalNotificationHandler()
+        kmApplocalNotificationHandler.dataConnectionNotificationHandler()
+        
+        if (KMUserDefaultHandler.isLoggedIn())
+        {
+            // Get login screen from storyboard and present it
+            if let viewController = UIStoryboard(name: "Main", bundle: nil)
+                .instantiateViewController(withIdentifier: "NavViewController") as? UINavigationController {
+                self.window?.makeKeyAndVisible();
+                self.window?.rootViewController!.present(viewController, animated:true, completion: nil)
+            }
+        }
+        
+        if (launchOptions != nil)
+        {
+            let dictionary = launchOptions?[UIApplication.LaunchOptionsKey.remoteNotification] as? NSDictionary
+            
+            if (dictionary != nil)
+            {
+                print("launched from push notification")
+                let kmPushNotificationService: KMPushNotificationService = KMPushNotificationService()
+                
+                let appState: NSNumber = NSNumber(value: 0 as Int32)
+                let _ = kmPushNotificationService.processPushNotification(launchOptions,updateUI:appState)
+            }
+        }
         return true
     }
-
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any])
+    {
+        print("Received notification :: \(userInfo.description)")
+        let kmPushNotificationService: KMPushNotificationService = KMPushNotificationService()
+        kmPushNotificationService.notificationArrived(to: application, with: userInfo)
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler
+        completionHandler: @escaping (UIBackgroundFetchResult) -> Void)
+    {
+        print("Received notification With Completion :: \(userInfo.description)")
+        let kmPushNotificationService: KMPushNotificationService = KMPushNotificationService()
+        kmPushNotificationService.notificationArrived(to: application, with: userInfo)
+        completionHandler(UIBackgroundFetchResult.newData)
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        print("APP_ENTER_IN_BACKGROUND")
+        NotificationCenter.default.post(name: Notification.Name(rawValue: "APP_ENTER_IN_BACKGROUND"), object: nil)
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        KMPushNotificationService.applicationEntersForeground()
+        print("APP_ENTER_IN_FOREGROUND")
+        
+        NotificationCenter.default.post(name: Notification.Name(rawValue: "APP_ENTER_IN_FOREGROUND"), object: nil)
+        UIApplication.shared.applicationIconBadgeNumber = 0
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        KMDbHandler.sharedInstance().saveContext()
     }
-
-
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data)
+    {
+        
+        print("DEVICE_TOKEN_DATA :: \(deviceToken.description)")  // (SWIFT = 3) : TOKEN PARSING
+        
+        var deviceTokenString: String = ""
+        for i in 0..<deviceToken.count
+        {
+            deviceTokenString += String(format: "%02.2hhx", deviceToken[i] as CVarArg)
+        }
+        print("DEVICE_TOKEN_STRING :: \(deviceTokenString)")
+        
+        if (KMUserDefaultHandler.getApnDeviceToken() != deviceTokenString)
+        {
+            let kmRegisterUserClientService: KMRegisterUserClientService = KMRegisterUserClientService()
+            kmRegisterUserClientService.updateApnDeviceToken(withCompletion: deviceTokenString, withCompletion: { (response, error) in
+                print ("REGISTRATION_RESPONSE :: \(String(describing: response))")
+            })
+        }
+    }
+    
+    func registerForNotification() {
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().requestAuthorization(options:[.badge, .alert, .sound]) { (granted, error) in
+                
+                if granted {
+                    DispatchQueue.main.async {
+                        UIApplication.shared.registerForRemoteNotifications()
+                    }
+                }
+            }
+        } else {
+            // Fallback on earlier versions
+            let settings = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            UIApplication.shared.registerUserNotificationSettings(settings)
+            UIApplication.shared.registerForRemoteNotifications()
+            
+        }
+    }
 }
-
--- ChatBot_IOS/ViewController.swift
+++ ChatBot_IOS/ViewController.swift
@@ -7,14 +7,67 @@
 //
 
 import UIKit
+import Kommunicate
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func OnGetStarted(_ sender: Any) {
+        let applicationId = AppDelegate.appId
+        setupApplicationKey(applicationId)
+        
+        let kmUser = userWithUserId(Kommunicate.randomId(), andApplicationId: applicationId)
+        registerUser(kmUser)
+        //launchConversation()
+    }
+    
+    private func setupApplicationKey(_ applicationId: String) {
+        guard !applicationId.isEmpty else {
+            fatalError("Please pass your AppId in the AppDelegate file.")
+        }
+        Kommunicate.setup(applicationId: applicationId)
+    }
+    
+    private func userWithUserId(
+        _ userId: String,
+        andApplicationId applicationId: String) -> KMUser {
+        let kmUser = KMUser()
+        kmUser.userId = userId
+        kmUser.applicationId = applicationId
+        return kmUser
+    }
+    
+    private func registerUser(_ kmUser: KMUser) {
+        activityIndicator.startAnimating()
+        Kommunicate.registerUser(kmUser, completion: {
+            response, error in
+            self.activityIndicator.stopAnimating()
+            guard error == nil else {
+                print("[REGISTRATION] Kommunicate user registration error: %@", error.debugDescription)
+                return
+            }
+            print("User registration was successful: %@ \(String(describing: response?.isRegisteredSuccessfully()))")
+            if let viewController = UIStoryboard(name: "Main", bundle: nil)
+                .instantiateViewController(withIdentifier: "NavViewController") as? UINavigationController {
+                self.present(viewController, animated:true, completion: nil)
+            }
+        })
+    }
+    
+    private func launchConversation() {
+        Kommunicate.createAndShowConversation(from: self, completion: {
+            error in
+            if error != nil {
+                print("Error while launching")
+            }
+        })
+    }
+    
 }
 
